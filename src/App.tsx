6c1aad5443beeb8a8b548f1ca684086ce6c43f92
evgeniypolupanov <evgeniypolupanov86@gmail.com>
2022-07-14 10:46:58 +0300

    add basic GraphQL queries

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import { GET_COMPANY_POSITIONS } from './graphql/queries';
 import { ListEntity } from './types';
 
 const App = () => {
-  const { loading, error, data } = useQuery(GET_COMPANY_POSITIONS, { variables: { name: 'employee'}});
-  console.log(data);
+  const { loading, error, data } = useQuery(GET_COMPANY_POSITIONS, { variables: { name: "employee" }, fetchPolicy: 'no-cache' });
 
   if (loading || error) {
     return null;
--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,19 +1,19 @@
 import { gql } from '@apollo/client';
 
-// export const GET_COMPANY_RELATIONS = gql`
-//     query applicantIndividualCompanyRelations($name: String!) {
-//         applicantIndividualCompanyRelations {
-//             data {
-//                 id
-//                 name
-//             }
-//         }
-//     }
-// `;
+export const GET_COMPANY_RELATIONS = gql`
+    query applicantIndividualCompanyRelations {
+        applicantIndividualCompanyRelations(where: {column: NAME, operator: LIKE, value: ""}) {
+            data {
+                id
+                name
+            }
+        }
+    }
+`;
 
 export const GET_COMPANY_POSITIONS = gql`
-    query applicantIndividualCompanyPositions($name: String!) {
-        applicantIndividualCompanyPositions(where: {name: $name}) {
+    query applicantIndividualCompanyPositions {
+        applicantIndividualCompanyPositions(where: {column: NAME, operator: LIKE, value: "Chief"}) {
             data {
                 id
                 name
